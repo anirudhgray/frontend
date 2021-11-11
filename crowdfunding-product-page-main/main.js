60d8ffad8e54c69d1b9649fa5c118c2bbaa3540f
anirudh <anirudh04mishra@gmail.com>
2021-11-12 01:39:46 +0530

    modal and radio toggle

--- crowdfunding-product-page-main/main.js
+++ crowdfunding-product-page-main/main.js
@@ -1,13 +1,20 @@
 let modalBG = document.querySelector(".modal-bg")
+let backModal = document.querySelector("#back-modal")
 let menuIcon = document.getElementById("menu-image")
 let bmIcon = document.querySelector("#bm-icon")
 let bmButton = document.querySelector("#bookmark-button")
+let backButtons = document.querySelectorAll(".back-buttons")
+let radioButtons = document.querySelectorAll(".radio")
 
 document.addEventListener("DOMContentLoaded", () => {
     modalBG.style.display = "none";
+    backModal.style.display = "none";
+    document.querySelectorAll(".radio-toggle-div").forEach(div => {
+        div.style.display = "none"
+    })
 })
 
-function modal() {
+function toggleModalBackground() {
     if (modalBG.style.display === "block") {
         modalBG.style.display = "none";
     } else {
@@ -16,7 +23,7 @@ function modal() {
 }
 
 document.getElementById("hamburger").addEventListener("click", () => {
-    modal()
+    toggleModal()
     let x = document.getElementById("navbar");
     if (x.className === "navbar") {
         x.className += " responsive";
@@ -41,3 +48,48 @@ function bookmark() {
 
 bmIcon.addEventListener("click", bookmark)
 bmButton.addEventListener("click", bookmark)
+
+function toggleModal() {
+    if (backModal.style.display === "block") {
+        backModal.style.display = "none";
+    } else if (backModal.style.display === "none") {
+        backModal.style.display = "block";
+    }
+}
+
+backButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        toggleModalBackground()
+        toggleModal()
+    })
+})
+
+document.getElementById("modal-cross").addEventListener("click", () => {
+    toggleModalBackground()
+    toggleModal()
+})
+
+function toggleRadio(evt) {
+    let button = evt.currentTarget
+    border = document.getElementById("b" + button.dataset.item)
+    div = document.getElementById("d" + button.dataset.item)
+    if (button.dataset.toggle === "off") {
+        border.style.borderColor = "hsl(176, 50%, 47%)"
+        border.style.boxShadow = "0 0 0 1px hsl(176, 50%, 47%)"
+        div.style.display = "flex"
+        button.dataset.toggle = "on"
+        button.src = "./images/radio-selected.svg"
+
+    } else {
+        border.style.borderColor = "hsl(0, 0%, 80%)"
+        border.style.boxShadow = "none"
+        border.style.borderWidth = "1px"
+        div.style.display = "none"
+        button.dataset.toggle = "off"
+        button.src = "./images/radio.svg"
+    }
+}
+
+radioButtons.forEach(button => {
+    button.addEventListener("click", (e) => toggleRadio(e))
+})
